fb8f5c417de6d28d70754a5c17741fb7e86a96d8
lizenan <lizenantyut@163.com>
2020-09-15 14:17:07 +0800

    删除失效方法和test2

--- 知识总结/Python/python和Excel结合/py_excel.py
+++ 知识总结/Python/python和Excel结合/py_excel.py
@@ -66,33 +66,6 @@ def excel_ys():
     """设置Excel样式"""
     pass
 
-
-def adjust_column_width_from_col(ws, min_row, min_col, max_col):
-    column_widths = []
-    for i, col in \
-            enumerate(
-                ws.iter_cols(min_col=min_col, max_col=max_col, min_row=min_row)
-            ):
-
-        for cell in col:
-            value = cell.value
-            if value is not None:
-
-                if isinstance(value, str) is False:
-                    value = str(value)
-
-                try:
-                    column_widths[i] = max(column_widths[i], len(value))
-                except IndexError:
-                    column_widths.append(len(value))
-
-    for i, width in enumerate(column_widths):
-        col_name = get_column_letter(min_col + i)
-        value = column_widths[i] + 2
-        ws.column_dimensions[col_name].width = value
-
-
-
 def data_to_excel(df,excle_name,sheet_name,excel_path=None):
     """
 
@@ -125,19 +98,6 @@ def data_to_excel(df,excle_name,sheet_name,excel_path=None):
         df.to_excel(excel_writer=excelWriter, sheet_name=sheet_name, index=False)
         excelWriter.save()
         excelWriter.close()
-
-def test2():
-    wb = openpyxl.load_workbook("test1.xlsx")
-    sheet = wb.get_sheet_by_name("2020-09-02")
-
-    for col in range(1,1000):
-      #col_value = data_dict[header_dict[str(col)]]
-      #ws.cell(row_num, col, col_value).number_format = fmt_acct
-      sheet.column_dimensions[get_column_letter(col)].auto_size = True
-    wb.save("test1.xlsx")
-    wb.close()
-    #wb.save("test1.xlsx")
-
 
 
 if __name__ == '__main__':
