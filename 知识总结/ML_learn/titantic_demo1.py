00625f8512b8281217d1f418b1e0c12e944a6714
lizenan <lizenantyut@163.com>
2020-09-27 22:26:24 +0800

    泰坦尼克号机器学习-delete

--- 知识总结/ML_learn/titantic_demo1.py
+++ 知识总结/ML_learn/titantic_demo1.py
@@ -7,7 +7,28 @@ import scipy as sp
 import IPython
 import sklearn
 import warnings
+#Common Model Algorithms
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from xgboost import XGBClassifier
 
+#Common Model Helpers
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+
+#Visualization
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+#from pandas.tools.plotting import scatter_matrix
+from pandas.plotting import scatter_matrix  #pandas 0.19之后用这个
+#Configure Visualization Defaults
+#%matplotlib inline = show plots in Jupyter Notebook browser
+mpl.style.use('ggplot')
+sns.set_style('white')
+pylab.rcParams['figure.figsize'] = 12,8
 
 def prepare_pack():
     """
